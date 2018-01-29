6953229f42898821885dd0e2c55dd594680fe1b5
Sean Van Osselaer <sean.vanosselaer@c02vm0gphtd8.myob.myobcorp.net>
2018-01-29 11:15:42 +1100

    Setting up brew, python, pip and ansible

---
+++ build.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+
+brew install python
+
+sudo easy_install pip
+
+pip install ansible==2.4.2.0
