cfb2dadff7f5c11fd473c515e07bac8e0055bcd4
Sean Van Osselaer <sean.vanosselaer@myob.com>
2018-04-09 17:09:40 +1000

    Removing need for ignore errors with virtualbox install

--- build.sh
+++ build.sh
@@ -3,6 +3,8 @@
 /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 
 brew install python
+brew install python@2
+brew link --overwrite python
 
 sudo easy_install pip
 
