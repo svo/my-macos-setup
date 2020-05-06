4ea768319d3dae99d1c5049cb09dd7e7d4f3ce8d
SVO <sean.vanosselaer@myob.com>
2020-05-07 09:32:16 +1000

    Moving to zsh from bash

--- build.sh
+++ build.sh
@@ -1,5 +1,3 @@
 #!/bin/bash
 
 ansible-playbook -i "localhost," -c local -K playbook.yml
-
-topgrade
--- prepare.sh
+++ prepare.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+chsh -s /bin/zsh
+
 /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)" &&
 
 brew update && brew upgrade && brew cask upgrade &&
