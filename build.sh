d8117c1cb71e7c70d27ccc99b753adef33190133
SVO <svo@qual.is>
2023-12-02 12:51:43 +1100

    Using python3.11 as the ansible interpreter

--- build.sh
+++ build.sh
@@ -2,4 +2,4 @@
 
 eval "$(/opt/homebrew/bin/brew shellenv)"
 
-ansible-playbook -i "localhost," -c local -K playbook.yml
+ansible-playbook -i "localhost," -c local -K -e 'ansible_python_interpreter=/opt/homebrew/bin/python3.11' playbook.yml
