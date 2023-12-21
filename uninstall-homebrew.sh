8856f0547618de7dca93331cc40e6eb03e8f15a9
SVO <svo@qual.is>
2023-12-21 15:57:32 +1100

    Trying another way to remove brew packages

--- uninstall-homebrew.sh
+++ uninstall-homebrew.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
-brew_path=`brew --prefix`
+brew remove --force $(brew list --formula)
+
+brew remove --cask --force $(brew list)
+
 curl -sLO https://raw.githubusercontent.com/Homebrew/install/master/uninstall.sh
 chmod +x ./uninstall.sh
 sudo ./uninstall.sh --force
