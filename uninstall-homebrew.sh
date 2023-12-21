a7866710ecd7620f4ca2dbc23b24e5fbe0fa7632
SVO <svo@qual.is>
2023-12-22 08:28:44 +1100

    Removing old homebrew uninstall script

--- uninstall-homebrew.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-brew remove --force $(brew list --formula)
-
-brew remove --cask --force $(brew list)
-
-curl -sLO https://raw.githubusercontent.com/Homebrew/install/master/uninstall.sh
-chmod +x ./uninstall.sh
-sudo ./uninstall.sh --force
