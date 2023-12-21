e986e58a4c8191fcef114e48f7a838db6d6386d6
SVO <svo@qual.is>
2023-12-22 08:28:00 +1100

    Allowing commands to run through in GitHub workflow to see result

--- bin/prepare-github.sh
+++ bin/prepare-github.sh
@@ -6,10 +6,12 @@ brew remove --cask --force $(brew list) &&
 
 brew update && brew upgrade && brew upgrade --cask &&
 
-brew doctor || true &&
+brew doctor
 
-brew install python@3.11 || brew link --overwrite python@3.11 &&
+brew install python@3.11
 
-brew install pipx &&
+brew link --overwrite python@3.11
+
+brew install pipx
 
 pipx install --include-deps ansible
