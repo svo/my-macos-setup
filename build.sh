503c0889c71bb1a939ab2ec8c4b72e5a9d9283ed
SVO <svo@qual.is>
2024-07-05 10:03:45 +1000

    Updated build script to ensure commands execute successfully

--- build.sh
+++ build.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 user=`whoami`
-export PATH="$PATH:/opt/homebrew/bin/:/Users/${user}/Library/Application Support/pipx/venvs/ansible/bin/"
+export PATH="$PATH:/opt/homebrew/bin/:/Users/${user}/Library/Application Support/pipx/venvs/ansible/bin/" &&
 
-brew_path=`brew --prefix`
+brew_path=`brew --prefix` &&
 
-eval "$(/${brew_path}/bin/brew shellenv)"
+eval "$(/${brew_path}/bin/brew shellenv)" &&
 
 ansible-playbook -i "localhost," -c local -K -e "ansible_python_interpreter=${brew_path}/bin/python3.11" playbook.yml
