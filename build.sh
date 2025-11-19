e5cf11fc614b978d2ff642fcac85e3f27071ffdc
SVO <svo@qual.is>
2025-11-20 04:44:51 +1100

    Trying to workaround sudo install for certain cask errors

--- build.sh
+++ build.sh
@@ -8,4 +8,10 @@ eval "$(/${brew_path}/bin/brew shellenv)" &&
 
 interpreter=`which python3`
 
-ansible-playbook -i "localhost," -c local -K -e "ansible_python_interpreter='${interpreter}'" playbook.yml
+if [ -t 0 ]; then
+  read -s -p "BECOME password: " BECOME_PASS
+  echo
+  export ANSIBLE_BECOME_PASS=$BECOME_PASS
+fi
+
+ansible-playbook -i "localhost," -c local -e "ansible_python_interpreter='${interpreter}'" playbook.yml
